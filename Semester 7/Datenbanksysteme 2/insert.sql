af1c3d452462671fdae3bae690f84ffb11b57ed0
JannisLiebscher <jannis-liebscher@gmx.de>
2023-10-08 17:28:30 +0200

    stuff

--- Semester 7/Datenbanksysteme 2/insert.sql
+++ Semester 7/Datenbanksysteme 2/insert.sql
@@ -20,14 +20,20 @@ VALUES ('Jane doe', 'München', 'Schwabing', 'Deutschland');
 
 -- Beispiel-Kunden
 INSERT INTO KUNDE (kunde_alter, kunde_name, kunde_land) VALUES
-(30, 'Max Mustermann', 'Deutschland'),
-(25, 'Maria Schmidt', 'Österreich'),
-(40, 'John Smith', 'USA'),
+(30, 'Max Mustermann', 'Deutschland');
+INSERT INTO KUNDE (kunde_alter, kunde_name, kunde_land) VALUES
+(25, 'Maria Schmidt', 'Österreich');
+INSERT INTO KUNDE (kunde_alter, kunde_name, kunde_land) VALUES
+(40, 'John Smith', 'USA');
+INSERT INTO KUNDE (kunde_alter, kunde_name, kunde_land) VALUES
 (28, 'Sophie Dupont', 'Frankreich');
 
 -- Beispiel-Verkäufer
 INSERT INTO VERKAEUFER (verkaeufer_geburtsdatum, verkaeufer_name, verkaeufer_fachgebiet) VALUES
-('1980-05-15', 'Anna Müller', 'Elektronik'),
-('1975-08-22', 'Peter Johnson', 'Bekleidung'),
-('1988-03-10', 'Elena Garcia', 'Haushaltsgeräte'),
-('1982-11-05', 'Hans Meier', 'Sportartikel');
+(TO_DATE('1980-05-15', 'YYYY-MM-DD'), 'Anna Müller', 'Elektronik');
+INSERT INTO VERKAEUFER (verkaeufer_geburtsdatum, verkaeufer_name, verkaeufer_fachgebiet) VALUES
+(TO_DATE('1975-08-22', 'YYYY-MM-DD'), 'Peter Johnson', 'Bekleidung');
+INSERT INTO VERKAEUFER (verkaeufer_geburtsdatum, verkaeufer_name, verkaeufer_fachgebiet) VALUES
+(TO_DATE('1988-03-10', 'YYYY-MM-DD'), 'Elena Garcia', 'Haushaltsgeräte');
+INSERT INTO VERKAEUFER (verkaeufer_geburtsdatum, verkaeufer_name, verkaeufer_fachgebiet) VALUES
+(TO_DATE('1982-11-05', 'YYYY-MM-DD'), 'Hans Meier', 'Sportartikel');
