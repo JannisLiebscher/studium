ad4359364c6504be4e69b6b799fd00d738152ae5
JannisLiebscher <jannis-liebscher@gmx.de>
2023-10-12 16:16:09 +0200

    db

--- Semester 7/Datenbanksysteme 2/cube.sql
+++ Semester 7/Datenbanksysteme 2/cube.sql
@@ -1,7 +1,7 @@
 DROP TABLE Verkauf_2DC;
 create Table Verkauf_2DC
 AS
-SELECT COALESCE(p.produkt_gruppe,'alle') as Gruppe, COALESCE(k.kunde_alter, -1) as "ALTER", SUM(v.verkauf_Anzahl) as anzahl
+SELECT COALESCE(p.produkt_gruppe,'alle') as Gruppe, COALESCE(TO_CHAR(k.kunde_alter), 'alle') as "ALTER", SUM(v.verkauf_Anzahl) as anzahl
 FROM Kunde k, Produkt p, Verkauf v
 WHERE v.verkauf_datum >= TO_DATE('2020-01-01', 'YYYY-MM-DD')
 AND v.produkt_ID = p.Produkt_ID
