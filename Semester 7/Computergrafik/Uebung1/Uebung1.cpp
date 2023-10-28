8e19b4acdc9a634138868576a37075c52d020275
JannisLiebscher <jannis-liebscher@gmx.de>
2023-10-28 15:50:25 +0200

    CG A1.2

--- Semester 7/Computergrafik/Uebung1/Uebung1.cpp
+++ Semester 7/Computergrafik/Uebung1/Uebung1.cpp
@@ -1,16 +1,16 @@
-﻿///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 //																	 //
-// Codegerüst für Vorlesung Computergraphik WS 2023/24 �bung 1       //
+// Codeger�st f�r Vorlesung Computergraphik WS 2023/24 �bung 1       //
 //										                             //
 ///////////////////////////////////////////////////////////////////////
 
 
-// Include-File für die Text Ein-/Ausgabe
+// Include-File f�r die Text Ein-/Ausgabe
 #include <iostream>
 using namespace std;
 
 
-// Include-File für die GLUT-Library
+// Include-File f�r die GLUT-Library
 #include "glut.h"
 
 
@@ -24,48 +24,48 @@ using namespace std;
 
 // Aufl�sungen der gesamten Textur
 // !!!ACHTUNG!!! nicht alle Texturaufl�sungen funktionieren!
-// Stichwort ungefähr: POT2 Problematik
+// Stichwort ungef�hr: POT2 Problematik
 #define TEX_RES_X 60
 #define TEX_RES_Y 60
 // Anzahl der Pixel der Textur
 #define TEX_RES TEX_RES_X*TEX_RES_Y
-// Achsenlänge der Textur (Achsen sind asymmetrisch von -TexRes#/2 bis TesRes#/2-1)
+// Achsenl�nge der Textur (Achsen sind asymmetrisch von -TexRes#/2 bis TesRes#/2-1)
 #define TEX_HALF_X TEX_RES_X/2
 #define TEX_HALF_Y TEX_RES_Y/2
 // Konvertiert Indices von (x,y) Koordinaten in ein lineares Array
 #define TO_LINEAR(x, y) (((x)) + TEX_RES_X*((y)))
 
-// globaler Speicher für Texturdaten
-char g_Buffer[3 * TEX_RES];
+// globaler Speicher f�r Texturdaten
+char g_Buffer[3*TEX_RES];
 // Textur ID Name
 GLuint g_TexID = 0;
 
-// Auflösung des Hauptfensters (kann sich durch User ändern)
+// Aufl�sung des Hauptfensters (kann sich durch User �ndern)
 int g_WinWidth = 800;
 int g_WinHeight = 800;
 
 // Funktion organisiert die Textur.
-// Kümmern Sie sich nicht weiter um diese Funktion, da
-// sie momentan nur ein notwendiges übel darstellt!
-void manageTexture()
+// K�mmern Sie sich nicht weiter um diese Funktion, da
+// sie momentan nur ein notwendiges �bel darstellt!
+void manageTexture () 
 {
-	glEnable(GL_TEXTURE_2D);
-	if (g_TexID == 0) glGenTextures(1, &g_TexID);
-	glBindTexture(GL_TEXTURE_2D, g_TexID);
-	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TEX_RES_X, TEX_RES_Y, 0, GL_RGB, GL_UNSIGNED_BYTE, g_Buffer);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glDisable(GL_TEXTURE_2D);
+	glEnable        (GL_TEXTURE_2D);
+	if (g_TexID==0) glGenTextures (1, &g_TexID);
+	glBindTexture   (GL_TEXTURE_2D, g_TexID);
+	glTexEnvf       (GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexImage2D    (GL_TEXTURE_2D, 0, GL_RGB, TEX_RES_X, TEX_RES_Y, 0, GL_RGB, GL_UNSIGNED_BYTE, g_Buffer);	
+	glBindTexture   (GL_TEXTURE_2D, 0);
+	glDisable       (GL_TEXTURE_2D);
 }
 
-// Callback Funktion um die Fenstergrößen anzupassen.
-// Auch diese Funktion ist ein notwendiges übel! Kümmern
+// Callback Funktion um die Fenstergr��en anzupassen.
+// Auch diese Funktion ist ein notwendiges �bel! K�mmern
 // Sie sich im Moment nicht weiter darum.
-void reshape(int w, int h)
+void reshape(int w, int h) 
 {
 
 	g_WinWidth = w;
@@ -74,9 +74,9 @@ void reshape(int w, int h)
 
 	glMatrixMode(GL_PROJECTION);			// Start modifying the projection matrix.
 	glLoadIdentity();						// Reset project matrix.
-	glOrtho(-w / 2, w / 2, -h / 2, h / 2, 0, 1);	// Map abstract coords directly to window coords.
+	glOrtho(-w/2, w/2, -h/2, h/2, 0, 1);	// Map abstract coords directly to window coords.
 
-	glutPostRedisplay();
+	glutPostRedisplay ();
 }
 
 
@@ -84,15 +84,15 @@ void reshape(int w, int h)
 //
 /////////////////////////////////////////////////////////////////////////////////////////
 //
-// Hier fängt der für Sie wirklich relevante Teil des Programms an.
+// Hier f�ngt der f�r Sie wirklich relevante Teil des Programms an.
 //
 
 
-// Eine überaus primitive Punktklasse
+// Eine �beraus primitive Punktklasse
 class Point {
 public:
 
-	Point(int x = 0, int y = 0) {
+	Point (int x=0, int y=0) {
 		this->x = x;
 		this->y = y;
 	}
@@ -100,11 +100,11 @@ public:
 	int x, y;
 };
 
-// Eine überaus primitive Farbklasse
+// Eine �beraus primitive Farbklasse
 class Color {
 public:
 
-	Color(float r = 1.0f, float g = 1.0f, float b = 1.0f) {
+	Color (float r=1.0f, float g=1.0f, float b=1.0f) {
 		this->r = r;
 		this->g = g;
 		this->b = b;
@@ -113,13 +113,13 @@ public:
 	float r, g, b;
 };
 
-// Funktion löscht den Bildschirm mit der angegebenen Farbe
+// Funktion l�scht den Bildschirm mit der angegebenen Farbe
 // Usage z.B.: clearImage (Color (1,1,1))
-// löscht den Bildschirm in Wei�.
+// l�scht den Bildschirm in Wei�.
 // Ohne Farbangabe ist der Standard Wei�
-void clearImage(Color c = Color()) {
-	for (int i = 0; i < TEX_RES; i++) {
-		g_Buffer[3 * i] = int(255.0 * c.r);
+void clearImage (Color c=Color()) {
+	for (int i=0; i<TEX_RES; i++) {
+		g_Buffer[3 * i    ] = int(255.0 * c.r);
 		g_Buffer[3 * i + 1] = int(255.0 * c.g);
 		g_Buffer[3 * i + 2] = int(255.0 * c.b);
 	}
@@ -131,7 +131,7 @@ void clearImage(Color c = Color()) {
 // Ohne Farbangabe ist die Standard-Malfarbe Schwarz
 //
 // Nutzen Sie diese Funktion ...
-void setPoint(Point p, Color c = Color(0, 0, 0)) {
+void setPoint (Point p, Color c=Color(0,0,0)) {
 	int x = p.x + TEX_HALF_X;
 	int y = p.y + TEX_HALF_Y;
 	if (x < 0 || y < 0 || x >= TEX_RES_X || y >= TEX_RES_Y) {
@@ -139,21 +139,21 @@ void setPoint(Point p, Color c = Color(0, 0, 0)) {
 		return;
 	}
 
-	g_Buffer[3 * TO_LINEAR(x, y)] = int(255.0 * c.r);
+	g_Buffer[3 * TO_LINEAR(x, y)    ] = int(255.0 * c.r);
 	g_Buffer[3 * TO_LINEAR(x, y) + 1] = int(255.0 * c.g);
 	g_Buffer[3 * TO_LINEAR(x, y) + 2] = int(255.0 * c.b);
 }
 
 //
-// ÜBUNG 1 AUFGABE 1:
+// �BUNG 1 AUFGABE 1:
 //
 // Diese Funktion soll eine Gerade zwischen den Punkten
 // p1 und p2 in der Farbe c malen. Benutzen Sie die Funktion
 // setPoint um die individuellen Punkte zu zeichnen.
-void bhamLine(Point p1, Point p2, Color c) {
+void bhamLine (Point p1, Point p2, Color c) {
 
 	// erster Punkt
-	setPoint(p1, c);
+	setPoint (p1, c);
 	int x1 = p1.x;
 	int y1 = p1.y;
 	int x2 = p2.x;
@@ -207,15 +207,14 @@ void bhamLine(Point p1, Point p2, Color c) {
 	int y = y1;
 
 	int plotX;
-	int plotY;
+	int plotY ;
 
 	while (x < x2) {
 		if (d >= 0) {
 			d += deltaNE;
 			x++;
 			y++;
-		}
-		else {
+		} else {
 			d += deltaE;
 			x++;
 		}
@@ -227,31 +226,65 @@ void bhamLine(Point p1, Point p2, Color c) {
 			if (!sPos) {
 				plotY = -x;
 			}
-		}
-		else if (!sPos) {
+		} else if (!sPos) {
 			plotY = -y;
 		}
 		setPoint(Point(plotX, plotY), c);
 	}
 
 	// letzter Punkt
-	setPoint(p2, c);
+	setPoint (p2, c); 
 }
-
+void translatePoint(Point plotPoint, Point center) {
+	setPoint(Point(plotPoint.x + center.x, plotPoint.y + center.y));
+}
 //
-// ÜBUNG 1 AUFGABE 2:
+// �BUNG 1 AUFGABE 2:
 //
 // Diese Funktion soll einen Kreis mit Radius r um den Punkt p
 // malen. Benutzen Sie die Funktion setPoint um die individuellen
 // Punkte zu zeichnen. Vergessen Sie nicht auch den Mittelpunkt zu
 // zeichnen!
-void bhamCircle(Point p, int r, Color c) {
+void bhamCircle (Point p, int r, Color c) {
 
-	// Mittelpunkt
-	setPoint(p, c);
+	int xx = p.x;
+	int yy = p.y;
 
-	// ...
+	int x, y, d, deltaSE, deltaE;
+	x = 0;
+	y = r;
+	d = 5 - 4 * r;
+	translatePoint(Point(x, y),p);
+	translatePoint(Point(x, -y),p);
+	translatePoint(Point(y, x),p);
+	translatePoint(Point(-y, x),p);
+
+	while (y > x) {
+		if (d >= 0) {
+			deltaSE = 4 * (2 * (x - y) + 5);
+			d += deltaSE;
+			x++;
+			y--;
+		} else {
+			deltaE = 4 * (2 * x + 3);
+			d += deltaE;
+			x++;
+		}
+
+		translatePoint(Point(x, y),p);
+		translatePoint(Point(x, -y),p);
+		translatePoint(Point(y, x),p);
+		translatePoint(Point(-y, x),p);
+
+		translatePoint(Point(-x, -y),p);
+		translatePoint(Point(-x, y),p);
+		translatePoint(Point(-y, -x),p);
+		translatePoint(Point(y, -x),p);
+	}
+	
 }
+
+
 
 void stuff() {
 	manageTexture();
@@ -274,13 +307,13 @@ void stuff() {
 	glFlush();
 }
 
-// Die Callback Funktion die für das eigentliche Malen
-// zuständig ist. Im Wesentlichen brauchen Sie sich nur
+// Die Callback Funktion die f�r das eigentliche Malen
+// zust�ndig ist. Im Wesentlichen brauchen Sie sich nur
 // um den Bereich zwischen den Kommentaren zu k�mmern,
-// alles andere ist wiederum ein notwendiges übel!
-void display(void)
+// alles andere ist wiederum ein notwendiges �bel!
+void display (void) 
 {
-	Color background(0.5, 0.5, 0.5);	// grauer Hintergrund
+	Color background(0.5,0.5,0.5);	// grauer Hintergrund
 	clearImage(background);				// alte Anzeige l�schen
 
 	//////////////////////////////////////////////////////////////////
@@ -289,7 +322,7 @@ void display(void)
 	// eine Gerade und einen Kreis zeichnen.
 	//
 	// 
-
+	
 	// Im Prinzip brauchen Sie hier keine �nderungen vorzunehmen, 
 	// es sei denn Sie wollen "spielen" :-D
 	//
@@ -297,13 +330,13 @@ void display(void)
 	//Point p1(-10, 20);				// ersten Punkt f�r Gerade definieren
 	//Point p2(20, -15);				// ebenso den zweiten Punkt
 
-	setPoint(Point(0, 0));
+	//setPoint(Point(0, 0));
 	Point p1(0, 0);
 
 
 	Point points[12] = {
 		Point(20, 0),
-		Point(17, 10),
+		Point(15, 10),
 		Point(10, 17),
 		Point(0, 20),
 		Point(-10, 17),
@@ -314,9 +347,10 @@ void display(void)
 		Point(0, -20),
 		Point(10, -17),
 		Point(17, -10),
-	};
+	};	
 	Color cRed(1, 0, 0);// Es soll eine rote Gerade sein ...
 
+	/*
 	int i = 0;
 	while (true) {
 		clearImage(background);
@@ -327,41 +361,42 @@ void display(void)
 		}
 		Sleep(500);
 		stuff();
-	}
+	}*/
 
-
+	
 	//bhamLine  (p1, p2, cRed);			// Gerade zeichnen ...
 
 	//setPoint(p1);
 	//setPoint(p2);
 
-	Point p(-3, -5);					// Mittelpunkt für Kreis definieren
-	int r = 17;							// Radius festlegen
-	Color cBlue(0, 0, 1);					// Es soll ein blauer Kreis sein ...
-	//bhamCircle (p, r, cBlue);			// Kreis zeichnen ...
+	Point p(20, 10);					// Mittelpunkt f�r Kreis definieren
+	int r = 5;							// Radius festlegen
+	Color cBlue(0,0,1);					// Es soll ein blauer Kreis sein ...
+	bhamCircle (p, r, cBlue);			// Kreis zeichnen ...
+	stuff();
 
 	//
 	// Ab hier sollten Sie nichts mehr �ndern!
 	//
 	//////////////////////////////////////////////////////////////////
 
-
+	
 }
 
 
 // Die Main-Funktion
-int main(int argc, char** argv) {
+int main (int argc, char **argv) {
 
-	glutInit(&argc, argv);
-	glutInitWindowSize(g_WinWidth, g_WinHeight);
-	glutCreateWindow("Computergrafik �bung 1: Bresenham (WS23/24)");
+	glutInit (&argc, argv);
+	glutInitWindowSize (g_WinWidth, g_WinHeight);
+	glutCreateWindow ("Computergrafik �bung 1: Bresenham (WS23/24)");
 
-	glutReshapeFunc(reshape);	// zuständig für Größen�nderungen des Fensters
-	glutDisplayFunc(display);	// zuständig für das wiederholte Neuzeichnen des Bildschirms
+	glutReshapeFunc (reshape);	// zust�ndig f�r Gr��en�nderungen des Fensters
+	glutDisplayFunc (display);	// zust�ndig f�r das wiederholte Neuzeichnen des Bildschirms
 
-	glutMainLoop();
+	glutMainLoop ();
 
-	glDeleteTextures(1, &g_TexID); // löscht die oben angelegte Textur
+	glDeleteTextures (1, &g_TexID); // l�scht die oben angelegte Textur
 
 	return 0;
 }
